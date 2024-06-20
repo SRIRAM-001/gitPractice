042cc1206f661f2865f80b9d9825b0974cf2905c
Sriram <sriramsivakumar23@gmail.com>
2024-06-20 10:39:35 +0530

    addition function added

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 class Main{
+    void add(int x,int y){
+        int ans = x+y;
+        System.out.println(ans);
+    }
     public static void main(String args[]){
         System.out.println("afsadf");
     }
