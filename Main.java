181475449ddad178ed8240152d6d71f1a3007692
Sriram <sriramsivakumar23@gmail.com>
2024-06-20 10:43:05 +0530

    add function created

--- Main.java
+++ Main.java
@@ -1,4 +1,8 @@
 class Main{
+    void add(int x){
+        int ans  = x+2;
+        System.out.println(ans);
+    }
     public static void main(String args[]){
         System.out.println("afsadf");
     }
